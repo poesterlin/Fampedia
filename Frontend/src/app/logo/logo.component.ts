135834c05e9a53382b777ade222d194f0e6949bd
Unknown <philip.oesterlin@gmail.com>
2019-02-28 15:37:08 +0100

    [angular] logo navigation bar and language switch

---
+++ Frontend/src/app/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { Component, AfterViewChecked, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'fampedia-logo',
+    templateUrl: './logo.component.html',
+    styleUrls: ['./logo.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class LogoComponent {
+    constructor() { }
+}
