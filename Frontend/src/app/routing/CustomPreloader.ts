0775d567f86cffb20fc7aea9f410b23694084e5d
Philip Oesterlin <philip.oesterlin@gmail.com>
2019-03-18 15:49:02 +0100

    [project] fix lazy loading and add delay

--- Frontend/src/app/routing/CustomPreloader.ts
+++ Frontend/src/app/routing/CustomPreloader.ts
@@ -1,9 +1,24 @@
 import { PreloadingStrategy, Route } from '@angular/router';
 
-import { Observable, of } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 
 export class CustomPreloader implements PreloadingStrategy {
   preload(route: Route, load: Function): Observable<any> {
-    return route.data && route.data.preload ? load() : of();
+    const loadRoute: Observable<any> | Function = (delay?: number) =>
+      delay
+        ? from(
+            new Promise(res =>
+              setTimeout(() => {
+                load();
+                console.log('delayed preload', route);
+                res();
+              }, delay * 1000)
+            )
+          )
+        : load();
+
+    return route.data && route.data.preload
+      ? loadRoute(route.data.delay)
+      : of(null);
   }
 }
