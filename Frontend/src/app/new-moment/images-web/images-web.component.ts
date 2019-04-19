6b519b22a5c576a5dabf8ca8546360f9c0b38950
Unknown <philip.oesterlin@gmail.com>
2019-04-19 13:33:52 +0200

    [newMoment] add image grid

--- Frontend/src/app/new-moment/images-web/images-web.component.ts
+++ Frontend/src/app/new-moment/images-web/images-web.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'fampedia-new-moment-images-web',
+  selector: 'fampedia-new-moment-images',
   templateUrl: './images-web.component.html',
   styleUrls: ['./images-web.component.scss']
 })
 export class ImagesWebComponent implements OnInit {
-
+  public images = new Array(100).fill(null).map(Math.random);
+  public takePicture = false;
   constructor() { }
 
   ngOnInit() {
--- Frontend/src/app/new-moment/new-moment.module.ts
+++ Frontend/src/app/new-moment/new-moment.module.ts
@@ -5,21 +5,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarModule } from '../navbar/navbar.module';
 import { MaterialModule } from '../material/material.module';
 import { DataComponent } from './data/data.component';
-import { ImagesNativeComponent } from './images-native/images-native.component';
 import { ImagesWebComponent } from './images-web/images-web.component';
 import { AccessComponent } from './access/access.component';
 import { PeopleSelectComponent } from './people-select/people-select.component';
 import { MomentCameraComponent } from './moment-camera/moment-camera.component';
+import { DirectivesModule } from '../directives/directives.module';
 
 const routes: Routes = [{ path: '**', component: NewMomentComponent }];
 
 @NgModule({
-  declarations: [NewMomentComponent, DataComponent, ImagesNativeComponent, ImagesWebComponent, AccessComponent, PeopleSelectComponent, MomentCameraComponent],
+  declarations: [NewMomentComponent, DataComponent, ImagesWebComponent, AccessComponent, PeopleSelectComponent, MomentCameraComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     NavbarModule,
     MaterialModule,
+    DirectivesModule
   ]
 })
 export class NewMomentModule { }
