a19d5c2a0bceffa1292cc73e559258a7c83d7e6e
Unknown <philip.oesterlin@gmail.com>
2019-04-21 16:24:40 +0200

    [newMoment] prefill date

--- Frontend/src/app/new-moment/data/data.component.ts
+++ Frontend/src/app/new-moment/data/data.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.component.scss']
 })
 export class DataComponent implements OnInit {
+  public today = `${this.two(new Date().getDate())} / ${this.two(new Date().getMonth())} / ${new Date().getFullYear()}`;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private two(n: number) {
+    return n < 10 ? "0" + n : n;
   }
 
 }
