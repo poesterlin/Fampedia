815d1f8470405b9068df9fa1b37353fe3ed49a2c
Philip Oesterlin <philip.oesterlin@gmail.com>
2019-04-28 13:20:56 +0200

    [newMoment] selectable images

--- Frontend/src/app/new-moment/images-web/images-web.component.ts
+++ Frontend/src/app/new-moment/images-web/images-web.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ImagesWebComponent implements OnInit {
   public images = new Array(100).fill(null).map(() => ({ i: Math.random(), data: "" }));
   public takePicture = false;
+  private selected: number[] = [];
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +17,18 @@ export class ImagesWebComponent implements OnInit {
   public addImages(images: string[]) {
     this.images.unshift(...images.map(data => ({ i: 0, data })));
     this.takePicture = false;
+  }
+
+  public select(idx: number) {
+    if (this.selected.includes(idx)) {
+      this.selected.splice(this.selected.findIndex(n => n === idx), 1);
+    } else {
+      this.selected.push(idx);
+    }
+  }
+
+  public isSelected(idx: number) {
+    return this.selected.includes(idx);
   }
 
 }
