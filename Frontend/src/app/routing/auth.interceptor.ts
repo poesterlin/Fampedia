6ec04d035a3d065ccca116a035c457de54d57266
Unknown <philip.oesterlin@gmail.com>
2019-05-01 23:54:48 +0200

    [login] redirect user on request error

--- Frontend/src/app/routing/auth.interceptor.ts
+++ Frontend/src/app/routing/auth.interceptor.ts
@@ -1,22 +1,32 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { LoginService } from '../login/login.service';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { IsLoggedIn } from './guard';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private loginService: LoginService) {}
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {  
-        const user = this.loginService.user$.getValue();
-        if (user) {
-            req = req.clone({
-                setHeaders: {
-                  token: user.token,
-                  user: user.username
-                }
-              });
+  constructor(private loginService: LoginService, private guard: IsLoggedIn) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const user = this.loginService.user$.getValue();
+    if (user) {
+      req = req.clone({
+        setHeaders: {
+          token: user.token,
+          user: user.username
         }
-        return next.handle(req)
+      });
     }
+    return next.handle(req).pipe(catchError((err, _) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          this.loginService.user$.next(undefined);
+          this.guard.canActivate();
+        }
+      }
+      return throwError('user unauthorised');
+    }));
+  }
 }
